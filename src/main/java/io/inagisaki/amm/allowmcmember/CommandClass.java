e89536318d3b1ea6161aee36f8fca3b2c48ffc10
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2023-05-29 10:57:13 +0900

    Command Permission fix

--- src/main/java/io/inagisaki/amm/allowmcmember/CommandClass.java
+++ src/main/java/io/inagisaki/amm/allowmcmember/CommandClass.java
@@ -20,37 +20,39 @@ public class CommandClass implements CommandExecutor, TabCompleter{
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (Arrays.asList("allowmcmember", "amm").contains(command.getName())) {
-            if (args.length == 0) {
-                return true;
-            } else {
-                if (args[0].equalsIgnoreCase("reload")) { //サブコマンドが「hello」かどうか
-                    sender.sendMessage("再読み込み中....");
-                    SettingManager.reloadConfig();
-                    for (Player player : Bukkit.getOnlinePlayers()) {
-                        Bukkit.getLogger().info(player.getName());
-                        LocalDate currentDate = LocalDate.now();
-                        DayOfWeek dayOfWeek = currentDate.getDayOfWeek();
-                        Bukkit.getLogger().info(dayOfWeek.toString());
-                        List<String> restrictionDays = SettingManager.getConfig().getStringList("RestrictionDay");
-                        if (restrictionDays.contains(dayOfWeek.toString())) {
-                            String userId = DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(player.getUniqueId());
-                            List<Guild> guilds = DiscordUtil.getJda().getGuilds();
-                            String guildId = guilds.get(0).getId().toString();
-                            Member member = DiscordUtil.getJda().getGuildById(guildId).getMemberById(userId);
-                            if (member != null) {
-                                Bukkit.getLogger().info(DiscordUtil.getTopRole(member).getId());
-                                List<String> AllowRole = SettingManager.getConfig().getStringList("AllowUser");
-                                String UserRole = DiscordUtil.getTopRole(member).getId();
-                                if (!AllowRole.contains(UserRole)) {
-                                    Bukkit.getLogger().info("許可されていないユーザーです。");
-                                    player.kickPlayer(SettingManager.getConfig().getString("KickMessage"));
+            if (sender.isOp()) {
+                if (args.length == 0) {
+                    return true;
+                } else {
+                    if (args[0].equalsIgnoreCase("reload")) { //サブコマンドが「hello」かどうか
+                        sender.sendMessage("再読み込み中....");
+                        SettingManager.reloadConfig();
+                        for (Player player : Bukkit.getOnlinePlayers()) {
+                            Bukkit.getLogger().info(player.getName());
+                            LocalDate currentDate = LocalDate.now();
+                            DayOfWeek dayOfWeek = currentDate.getDayOfWeek();
+                            Bukkit.getLogger().info(dayOfWeek.toString());
+                            List<String> restrictionDays = SettingManager.getConfig().getStringList("RestrictionDay");
+                            if (restrictionDays.contains(dayOfWeek.toString())) {
+                                String userId = DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(player.getUniqueId());
+                                List<Guild> guilds = DiscordUtil.getJda().getGuilds();
+                                String guildId = guilds.get(0).getId().toString();
+                                Member member = DiscordUtil.getJda().getGuildById(guildId).getMemberById(userId);
+                                if (member != null) {
+                                    Bukkit.getLogger().info(DiscordUtil.getTopRole(member).getId());
+                                    List<String> AllowRole = SettingManager.getConfig().getStringList("AllowUser");
+                                    String UserRole = DiscordUtil.getTopRole(member).getId();
+                                    if (!AllowRole.contains(UserRole)) {
+                                        Bukkit.getLogger().info("許可されていないユーザーです。");
+                                        player.kickPlayer(SettingManager.getConfig().getString("KickMessage"));
+                                    }
                                 }
                             }
                         }
+                        sender.sendMessage("再読み込みに成功しました。");
                     }
-                    sender.sendMessage("再読み込みに成功しました。");
+                    return true;
                 }
-                return true;
             }
         }
         return false;
